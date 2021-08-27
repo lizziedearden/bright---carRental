20d659c6763e8325b48c266feb7e17f85e027c39
Sophia210997 <sophsecretary@gmail.com>
2021-08-27 15:21:23 +0100

    Added rented/available methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,8 @@
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] arguments){
+    public static void main(String[] arguments) {
         Car car1 = new Car("Tesla", 001, 500, true);
         Car car2 = new Car("Ferrari", 002, 700, false);
         Car car3 = new Car("Mercedes", 003, 400, true);
@@ -12,5 +13,31 @@ public class Main {
         cars.add(car3);
 
         System.out.println(cars.toString());
+        System.out.println(rentedCars(cars));
+        System.out.println(availableCars(cars));
+
+
+    }
+
+    public static ArrayList<Car> rentedCars(ArrayList<Car> cars) {
+        ArrayList<Car> rentedCars = new ArrayList<>();
+        cars.forEach(car -> {
+            if (car.isRented() == true) {
+                rentedCars.add(car);
+            }
+
+        });
+        return rentedCars;
+    }
+
+    public static ArrayList<Car> availableCars(ArrayList<Car> cars) {
+        ArrayList<Car> availableCars = new ArrayList<>();
+        cars.forEach(car -> {
+            if (car.isRented() == false) {
+                availableCars.add(car);
+            }
+
+        });
+        return availableCars;
     }
 }
