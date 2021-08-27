e05049995adaf56d5d2725363ab8fbadc18005ce
lizziedearden <87985465+lizziedearden@users.noreply.github.com>
2021-08-27 16:58:14 +0100

    initial user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
-import java.sql.Array;
+
 import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] arguments) {
@@ -12,20 +14,46 @@ public class Main {
         cars.add(car2);
         cars.add(car3);
 
-        System.out.println(cars.toString());
-        System.out.println(rentedCars(cars));
-        System.out.println(availableCars(cars));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you a manager or a customer? m/c");
+        String managerCustomer = scanner.nextLine().toLowerCase();
+        if(managerCustomer.equals("m")){
+            System.out.println("Would you like to: \n (a)See list of cars \n (b)See list of rented cars \n (c)See list of available cars \n (d)Add a car \n (e)Remove a car \n Select a/b/c/d/e");
+            String managerAnswer = scanner.nextLine().toLowerCase();
+            switch (managerAnswer) {
+                case "a":
+                    System.out.println(cars.toString());
+                    break;
+                case "b":
+                    System.out.println(rentedCars(cars));
+                    break;
+                case "c":
+                    System.out.println(availableCars(cars));
+                    break;
+                default:
+                    System.out.println("Input invalid");
+                    break;
+            }
+        } else if(managerCustomer.equals("c")){
+            System.out.println("customer");
+        } else {
+            System.out.println("Your input was invalid!");
+        }
 
-        Car car4 = new Car("BMW", 004, 600, false);
-        CarRentalDB.addCar(cars, car4);
-        System.out.println(cars);
 
-        Car car5 = new Car("Honda", 005, 300, true);
-        CarRentalDB.addCar(cars, car5);
-        System.out.println(cars);
 
-        CarRentalDB.removeCar(cars, car5);
-        System.out.println(cars);
+
+
+//        Car car4 = new Car("BMW", 004, 600, false);
+//        CarRentalDB.addCar(cars, car4);
+//        System.out.println(cars);
+//
+//        Car car5 = new Car("Honda", 005, 300, true);
+//        CarRentalDB.addCar(cars, car5);
+//        System.out.println(cars);
+//
+//        CarRentalDB.removeCar(cars, car5);
+//        System.out.println(cars);
 
     }
 
